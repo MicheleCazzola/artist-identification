feat: modified model and fixed normalization, added script for latency evaluation

scripts/stats/stats.py
@@ -11,7 +11,7 @@ import json
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 NUM_WORKERS = 2 if DEVICE == "cuda" else 0
 DEFAULT_ROOT = "./scripts/stats/images/artist_dataset"
-OUTFILE = "stats.json"
+OUTFILE = "stats_all.json"
 
 def load_data():
     
@@ -31,12 +31,13 @@ def load_data():
     
     return dataset, dataloader
 
+@torch.no_grad()
 def compute_stats(dataset: ArtistDataset, dataloader: DataLoader):
     
     assert isinstance(dataset, ArtistDataset), "Dataset must be of type ArtistDataset"
     
     keys = ["data_length", "categories_length", "avg_categories_size",
-             "categories_size", "mean", "devstd", "dimensions",
+             "categories_size", "mean", "std", "dimensions",
              "min_height", "max_height", "min_width", "max_width"]
     
     num_authors = len(dataset.categories)
@@ -86,9 +87,10 @@ def compute_stats(dataset: ArtistDataset, dataloader: DataLoader):
     
     return dict(zip(
         keys, 
-        [data_len, num_authors, mean(labels_counts.values()),
-        labels_counts, avg.flatten().tolist(), std.flatten().tolist(), dimensions,
-        h_min, h_max, w_min, w_max]
+        [
+            data_len, num_authors, mean(labels_counts.values()),
+            labels_counts, avg.flatten().tolist(), std.flatten().tolist(),
+            dimensions, h_min, h_max, w_min, w_max]
     ))
 
 print(f"Device: {DEVICE}\nWorkers: {NUM_WORKERS}")

src/mutlti_branch_nn.py
@@ -1,3 +1,4 @@
+import time
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -12,17 +13,17 @@ class ResidualBlock(nn.Module):
         super(ResidualBlock, self).__init__()
         bottleneck_channels = out_channels // 4
 
-        self.conv1 = nn.Conv2d(in_channels, bottleneck_channels, kernel_size=1, stride=stride)
+        self.conv1 = nn.Conv2d(in_channels, bottleneck_channels, kernel_size=1, stride=stride, bias=False)
         self.bn1 = nn.BatchNorm2d(bottleneck_channels)
-        self.conv2 = nn.Conv2d(bottleneck_channels, bottleneck_channels, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(bottleneck_channels, bottleneck_channels, kernel_size=3, padding=1, bias=False)
         self.bn2 = nn.BatchNorm2d(bottleneck_channels)
-        self.conv3 = nn.Conv2d(bottleneck_channels, out_channels, kernel_size=1)
+        self.conv3 = nn.Conv2d(bottleneck_channels, out_channels, kernel_size=1, bias=False)
         self.bn3 = nn.BatchNorm2d(out_channels)
 
         self.shortcut = nn.Sequential()
         if stride != 1 or in_channels != out_channels:
             self.shortcut = nn.Sequential(
-                nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=stride),
+                nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=stride, bias=False),
                 nn.BatchNorm2d(out_channels)
             )
 
@@ -90,10 +91,17 @@ class HandCraftedFeatures(nn.Module):
         return torch.tensor(features, dtype=torch.float32, device=x.device)
 
 class MultibranchNetwork(nn.Module):
-    def __init__(self, out_classes, use_stn=True):
+    def __init__(
+        self,
+        out_classes: int,
+        use_stn: bool = True,
+        use_handcrafted: bool = True
+    ):
         super(MultibranchNetwork, self).__init__()
         
         self.out_classes = out_classes
+        self.use_stn = use_stn
+        self.use_handcrafted = use_handcrafted
 
         # ROI proposal module
         self.roi_proposal = ROIsProposal(use_stn=use_stn)
@@ -102,9 +110,6 @@ class MultibranchNetwork(nn.Module):
         self.branch1 = self._make_branch()
         self.branch2 = self._make_branch()
         self.branch3 = self._make_branch()
-
-        # Hand-crafted feature injection
-        self.hand_crafted_features = HandCraftedFeatures()
 
         # Concatenation processing
         self.join_layer = ResidualBlock(768, 256, stride=1)
@@ -115,18 +120,21 @@ class MultibranchNetwork(nn.Module):
         self.resblock3 = self._make_residual_block(1024, 2048, num_blocks=3, stride=2)
 
         self.avg_pool = nn.AdaptiveAvgPool2d((1, 1))
-
-        self.fc_artist = nn.Linear(2048 + 4056, out_classes)
+        
+        # Hand-crafted feature injection
+        self.hand_crafted_features = HandCraftedFeatures() if use_handcrafted else None
+        
+        # Final classification layer
+        self.fc_artist = nn.Linear(2048 + 4056, out_classes) if use_handcrafted else nn.Linear(2048, out_classes)
 
     def _make_branch(self):
-        layers = [
+        return nn.Sequential(
             nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3),
             nn.BatchNorm2d(64),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=3, stride=2, padding=1),
             self._make_residual_block(64, 256, num_blocks=3, stride=1)
-        ]
-        return nn.Sequential(*layers)
+        )
 
     def _make_residual_block(self, in_channels, out_channels, num_blocks, stride):
         strides = [stride] + [1] * (num_blocks - 1)
@@ -154,6 +162,7 @@ class MultibranchNetwork(nn.Module):
         out = self.resblock1(out)
         out = self.resblock2(out)
         out = self.resblock3(out)
+        
         out = self.avg_pool(out)
         out = torch.flatten(out, 1)
 
@@ -168,3 +177,29 @@ class MultibranchNetwork(nn.Module):
         artist = self.fc_artist(out)
         
         return artist
+    
+# model = MultibranchNetwork(out_classes=161)
+
+# modules = [(name, sum(p.numel() for p in module.parameters())) for name, module in model.named_children()]
+
+# for name, size in modules:
+#     print(f"{name}: {size * 4 / 1024**2:.2f} MB")
+
+# tot_params = sum(p.numel() for p in model.parameters())
+# print(f"Total size: {tot_params * 4 / 1024**2:.2f} MB")
+
+# x = [torch.randn(4, 3, 512, 512) for _ in range(10)]
+
+# logelapsed = 0
+# start = time.time()
+# with torch.no_grad():
+#     for (step, batch) in enumerate(x):
+#         model(batch)
+        
+#         logtime = time.time()
+#         if (step + 1) % 2 == 0:
+#             print(f"Batch {step + 1}/{len(x)}") 
+#         logelapsed += time.time() - logtime
+    
+
+# print(f"Elapsed time per-batch: {(time.time() - start) / len(x):.3f} s, for log: {logelapsed:.3f} s")

src/train.py
@@ -10,7 +10,7 @@ import torch.optim as optim
 import torch.nn as nn
 from torch.backends import cudnn
 from src.config import Config
-from src.network import MultibranchNetwork
+from src.mutlti_branch_nn import MultibranchNetwork
 from torch.utils.data import DataLoader
 from torchvision.transforms import Compose
 from torchmetrics.classification import Accuracy, MulticlassAccuracy
@@ -94,15 +94,14 @@ class Trainer:
         trainloader: DataLoader,
         validloader: DataLoader,
         testloader: DataLoader,
-        device: torch.device,
-        log_frequency: int
     ):
-        self.model: MultibranchNetwork = model.to(device)
+        self.model: MultibranchNetwork = model
         self.trainloader: DataLoader = trainloader
         self.validloader: DataLoader = validloader
         self.testloader: DataLoader = testloader
-        self.device: torch.device = device
-        self.log_frequency: int = log_frequency
+        
+        self.device: torch.device = None
+        self.log_frequency: int = None
         
         self.num_epochs: int = None
         self.lr: float = None
@@ -110,6 +109,7 @@ class Trainer:
         self.step_size: int = None
         self.gamma: float = None
         self.weight_decay: float = None
+        
         self.aug_train: Compose = None
         self.norm_eval: transforms.Normalize = None
         
@@ -125,29 +125,29 @@ class Trainer:
         self.training_results: TrainingResult = None
         self.test_results: EvaluationResult = None
         
-    def set_params(
-        self,
-        num_epochs: int,
-        lr: float,
-        momentum: float,
-        step_size: int,
-        gamma: float,
-        weight_decay: float,
-        top_k: int,
-        aug_train: Compose,
-        norm_eval: transforms.Normalize
-    ):
-        self.num_epochs = num_epochs
-        self.lr = lr
-        self.momentum = momentum
-        self.step_size = step_size
-        self.gamma = gamma
-        self.weight_decay = weight_decay
-        self.metrics = TrainingMetrics(num_classes=self.model.out_classes, top_k=top_k).to(self.device)
-        self.aug_train = aug_train
-        self.norm_eval = norm_eval
+    def build(self, cfg: Config):
+        self.device = cfg.device
+        self.log_frequency = cfg.log_frequency
+        self.num_epochs = cfg.num_epochs
+        self.lr = cfg.lr
+        self.momentum = cfg.momentum
+        self.step_size = cfg.scheduler_step_size
+        self.gamma = cfg.scheduler_gamma
+        self.weight_decay = cfg.weight_decay
+        self.top_k = cfg.top_k
         
-    def build_trainer(
+        self._prepare_training(cfg.criterion, cfg.optimizer, cfg.scheduler)
+        
+        self.metrics = TrainingMetrics(num_classes=cfg.num_classes, top_k=cfg.top_k)
+        
+        self.model = self.model.to(self.device)
+        self.metrics = self.metrics.to(self.device)
+        
+    def add_aug_transforms(self, transforms: dict):
+        self.aug_train = transforms["train"]
+        self.norm_eval = transforms["val"]
+        
+    def _prepare_training(
         self,
         criterion: str,
         optimizer: str,
@@ -241,11 +241,11 @@ class Trainer:
         self.training_results = TrainingResult(train_losses, train_accuracies, val_losses, val_accuracies, best_num_epochs)
     
     def training_eval(self) -> EvaluationResult:
-        losses, top_1_accuracies, _, _ = self.evaluate(self.trainloader, normalize=True)
+        losses, top_1_accuracies, _, _ = self.evaluate(self.trainloader)
         return losses, top_1_accuracies
     
     def validate(self) -> EvaluationResult:
-        losses, top_1_accuracies, _, _ = self.evaluate(self.validloader, normalize=True)
+        losses, top_1_accuracies, _, _ = self.evaluate(self.validloader)
         return losses, top_1_accuracies
     
     def test(self) -> EvaluationResult:
@@ -259,7 +259,7 @@ class Trainer:
         self.test_results = test_result
 
     @torch.no_grad()
-    def evaluate(self, dataloader: DataLoader, normalize: bool = False) -> EvaluationResult:
+    def evaluate(self, dataloader: DataLoader) -> EvaluationResult:
         
         self.model.eval()
 
@@ -273,8 +273,7 @@ class Trainer:
 
             data_len += inputs.size(0)
             
-            if self.norm_eval is not None and normalize:
-                inputs = torch.stack([self.norm_eval(input_img) for input_img in inputs])
+            inputs = torch.stack([self.norm_eval(input_img) for input_img in inputs])
 
             inputs = inputs.to(self.device)
             labels = labels.to(self.device)
@@ -318,15 +317,15 @@ class Trainer:
             plt.plot(val, label=label)
             plt.title(f"{name} vs. epochs")
         
-        plt.xticks(ticks=range(len(values)+1), labels=range(1, len(values)+2))
+        plt.xticks(ticks=range(self.num_epochs), labels=range(1, self.num_epochs + 1))
         plt.xlabel("Epochs")
         plt.ylabel(name)
         plt.grid(which="both")
         plt.legend()
-        plt.xlim(0, len(values)+0.2)
+        plt.xlim(0, self.num_epochs - 0.8)
         
         if name == "Accuracy":
-            plt.ylim(-0.1,1.1)
+            plt.ylim(-0.01, max(values[0] + values[1]) + 0.1)
             
         f.savefig(f"{save_path}/{name.lower()}_{file_id}.png")
     