5d2b3f0b99b8d6e8597518c4e7226b90cf8e91b6
Michele Cazzola <michele.cazzola2407@gmail.com>
2024-12-18 19:19:51 +0100

    fix in backbone type

--- src/network.py
+++ src/network.py
@@ -11,13 +11,16 @@ from enum import Enum
 from torchvision.models import shufflenet_v2_x0_5
 from torchvision.models import mnasnet0_5
 
-class BackboneType(Enum):
+class BackboneType(str, Enum):
     RESNET18 = "resnet18"
     MOBILENET_V3_SMALL = "mobilenet_v3_small"
     EFFICIENTNET_B0 = "efficientnet_b0"
     REGNET_X_400MF = "regnet_x_400mf"
     SHUFFLENET_V2_X0_5 = "shufflenet_v2_x0_5"
     MNASNET0_5 = "mnasnet0_5"
+    
+    # def __str__(self):
+    #     return self.value
 
 class BottleneckBlock(nn.Module):
     """
